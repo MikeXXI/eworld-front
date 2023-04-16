895a0eb0e75758df01f565390c59196dc2046e7f
yan <yan.ledoux@icloud.com>
2023-04-17 01:07:12 +0200

    materiel UI

--- src/components/forminscription.jsx
+++ src/components/forminscription.jsx
@@ -3,6 +3,7 @@ import '../styles/forminscription.css'
 import logo from '../assets/logoeworld.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faLock} from '@fortawesome/free-solid-svg-icons';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Inscription = () => {
     const [nom, setNom] = useState('');
@@ -10,6 +11,21 @@ const Inscription = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const useStyles = makeStyles({
+        title: {
+            color: '#5E8CFF',
+            fontSize: 60,
+            fontWeight: 'normal'
+        },
+        inputIcon: {
+            display: 'inline-block',
+            width: 40,
+            textAlign: 'center',
+            color: 'white',
+        },
+    });
+
+    const classes = useStyles();
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('Soumission du formulaire avec les données:', { nom, prenom, email, password });
@@ -19,7 +35,7 @@ const Inscription = () => {
         <div className="divinscription">
             <form onSubmit={handleSubmit}>
                 <img src={logo} alt='logo e-world' className='lmj-logo' />
-                <h1>Inscription</h1>
+                <h1 className={classes.title}>Inscription</h1>
                 <div className="inputlabel">
                 <div>
                     <label htmlFor="username" className="icon-label">
@@ -29,19 +45,19 @@ const Inscription = () => {
                 </div>
                 <div>
                     <label htmlFor="username" className="icon-label">
-                        <FontAwesomeIcon icon={faUser} />
+                        <FontAwesomeIcon icon={faUser} className={classes.inputIcon}/>
                     </label>
                     <input placeholder="Prénom" type="text" id="prenom" value={prenom} onChange={(e) => setPrenom(e.target.value)} />
                 </div>
                 <div>
                     <label htmlFor="username" className="icon-label">
-                        <FontAwesomeIcon icon={faEnvelope} />
+                        <FontAwesomeIcon icon={faEnvelope} className={classes.inputIcon}/>
                     </label>
                     <input placeholder="Email" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div>
                     <label htmlFor="password" className="icon-label">
-                        <FontAwesomeIcon icon={faLock} />
+                        <FontAwesomeIcon icon={faLock} className={classes.inputIcon}/>
                     </label>
                     <input placeholder="Mot de passe" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
