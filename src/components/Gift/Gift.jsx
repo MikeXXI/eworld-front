1d270e8aa982243f157b1d5577bf3ef63bb72ff5
k2 <mike.dje@outlook.fr>
2023-05-02 14:54:28 +0200

    gift and task with circle chargement

--- src/components/Gift/Gift.jsx
+++ src/components/Gift/Gift.jsx
@@ -1,56 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Grid, Container } from '@mui/material';
-import Giftlist from './Giftlist';
+import React, { useEffect, useState } from "react";
+import CircularProgress from '@mui/material/CircularProgress';
+import { Box, Grid, Container } from "@mui/material";
+import Giftlist from "./Giftlist";
 // import jsonFile from "../../data/gift.json";
 import logo from "../../assets/logoeworld.png";
-import { Link } from 'react-router-dom';
-import '../../styles/style.css';
-
+import { Link } from "react-router-dom";
+import "../../styles/style.css";
 
 function Gift() {
-    const [giftData, setGiftData] = useState([]);
-    const [load, isLoad] = useState(true);
+  const [giftData, setGiftData] = useState([]);
+  const [load, isLoad] = useState(true);
 
-    //const data = jsonFile;
-    //useEffect(function () {
-    //    isLoad(false);
-    //    setGiftData(data);
-    //}, [])
+  //const data = jsonFile;
+  //useEffect(function () {
+  //    isLoad(false);
+  //    setGiftData(data);
+  //}, [])
 
+  useEffect(function () {
+    isLoad(true);
+    fetch("https://eworld-api.osc-fr1.scalingo.io/api/gifts")
+      .then((res) => res.json())
+      .then((data) => {
+        isLoad(false);
+        setGiftData(data);
+      });
+  }, []);
 
-    useEffect(function () {
-        isLoad(true)
-        fetch("https://eworld-api.osc-fr1.scalingo.io/api/gifts")
-          .then(res => res.json())
-          .then(data => {
-            isLoad(false)
-            setGiftData(data)
-          }
-          )
-      }, [])
-   
-
-    return (
-        <Container>
-            <div className="divHeader">
-                <img src={logo} alt='logo e-world'/>
-                <Link to={`/gift/add`} className="bouttonadd">
-                    Ajouter un Cadeau
-                </Link>
-                <Link to={`/`} className="bouttonadd">
-                    Retour accueil
-                </Link>
-            </div>
-      {load ?
+  return (
+    <Container>
+      <div className="divHeader">
+        <img src={logo} alt="logo e-world" />
+        <Link to={`/gift/add`} className="bouttonadd">
+          Ajouter un Cadeau
+        </Link>
+        <Link to={`/`} className="bouttonadd">
+          Retour accueil
+        </Link>
+      </div>
+      {load ? (
         <Grid>
-          <Box>
-            En cour de chargement ...
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress />
           </Box>
-        </Grid> :
+        </Grid>
+      ) : (
         <Grid>
           <Giftlist giftData={giftData} />
         </Grid>
-      }
+      )}
     </Container>
   );
 }
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Grid, Container } from '@mui/material';
-import Tasklist from './Tasklist';
+import React, { useEffect, useState } from "react";
+import CircularProgress from '@mui/material/CircularProgress';
+import { Box, Grid, Container } from "@mui/material";
+import Tasklist from "./Tasklist";
 // import jsonFile from '../../data/task.json';
 import logo from "../../assets/logoeworld.png";
-import { Link } from 'react-router-dom';
-import '../../styles/style.css';
-
-
+import { Link } from "react-router-dom";
+import "../../styles/style.css";
 
 function Task() {
-    const [taskData, setTaskData] = useState([]);
-    const [load, isLoad] = useState(true);
+  const [taskData, setTaskData] = useState([]);
+  const [load, isLoad] = useState(true);
 
-     //const data = jsonFile;
-     //useEffect(function () {
-     //    isLoad(false);
-     //    setTaskData(data);
-     //}, [])
+  //const data = jsonFile;
+  //useEffect(function () {
+  //    isLoad(false);
+  //    setTaskData(data);
+  //}, [])
 
-    useEffect(function () {
-       isLoad(true)
-      fetch("https://eworld-api.osc-fr1.scalingo.io/api/tasks")
-          .then(res => res.json())
-          .then(data => {
-           isLoad(false)
-           setTaskData(data)
-           console.log(data)
-         }
-         )
-      }, [])
+  useEffect(function () {
+    isLoad(true);
+    fetch("https://eworld-api.osc-fr1.scalingo.io/api/tasks")
+      .then((res) => res.json())
+      .then((data) => {
+        isLoad(false);
+        setTaskData(data);
+        console.log(data);
+      });
+  }, []);
 
-    return (
-        <Container>
-            <div className="divHeader">
-            <img src={logo} alt='logo e-world'/>
-            <Link to={`/task/add`} className="bouttonadd">
-                Ajouter une tâche
-            </Link>
-                <Link to={`/`} className="bouttonadd">
-                    Retour accueil
-                </Link>
-            </div>
-            {load ?
+  return (
+    <Container>
+      <div className="divHeader">
+        <img src={logo} alt="logo e-world" />
+        <Link to={`/task/add`} className="bouttonadd">
+          Ajouter une tâche
+        </Link>
+        <Link to={`/`} className="bouttonadd">
+          Retour accueil
+        </Link>
+      </div>
+      {load ? (
         <Grid>
-          <Box>
-            En cour de chargement ...
+          <Box sx={{ display: "flex"}}>
+            <CircularProgress />
           </Box>
-        </Grid> :
-        <Grid>
-          <Tasklist taskData={taskData}  />
         </Grid>
-
-      }
+      ) : (
+        <Grid>
+          <Tasklist taskData={taskData} />
+        </Grid>
+      )}
     </Container>
-    );
+  );
 }
 
 export default Task;
