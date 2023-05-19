4558cdae2798f4852bbac10b6c4e803ca3a23572
yan <yan.ledoux@icloud.com>
2023-05-19 19:05:28 +0200

    optimisation du code

--- src/components/Gift/Gift.jsx
+++ src/components/Gift/Gift.jsx
@@ -12,7 +12,6 @@ import { ToastContainer } from 'react-toastify';
 import Addgift from "../Gift/Addgift";
 import Header from '../Header';
 
-
 function Gift() {
     const [giftData, setGiftData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -60,41 +59,21 @@ function Gift() {
                     </Box>
                 </Grid>
             ) : (
-                <div style={{
-                    position: 'fixed',
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    display: 'flex',
-                    justifyContent: 'space-between'
-                }}
-                     className="container_weather_taskgift">
-                    <div style={{display: 'flex', flexDirection: 'column', marginLeft: "15%"}}
-                         className="weather_sphere">
+                <div style={{ position: 'fixed', left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'space-between' }} className="container_weather_taskgift">
+                    <div style={{ display: 'flex', flexDirection: 'column', marginLeft: "15%" }} className="weather_sphere">
                         <Weather/>
-                        <img src={sphere} style={{width: "20vw", borderRadius: 100, marginTop: "50px"}} alt="sphere"/>
+                        <img src={sphere} style={{ width: "20vw", borderRadius: 100, marginTop: "50px" }} alt="sphere"/>
                     </div>
-                    <div style={{display: 'flex', flexDirection: 'column'}} className="container_task_gift">
-                        <div style={{display: "flex", alignItems: "center"}} className="container_add">
+                    <div style={{ display: 'flex', flexDirection: 'column' }} className="container_task_gift">
+                        <div style={{ display: "flex", alignItems: "center" }} className="container_add">
                             <Fab color="primary" aria-label="add" onClick={handleOpenModal}>
                                 <AddIcon/>
                             </Fab>
-                            <Link to={`/task`} style={{marginLeft: "50px", textDecoration: "none"}}>
-                                <p style={{
-                                    fontSize: "40px",
-                                    margin: "20px",
-                                    color: "white",
-                                    fontWeight: "100"
-                                }}>Tâches</p>
+                            <Link to={`/task`} style={{ marginLeft: "50px", textDecoration: "none" }}>
+                                <p style={{ fontSize: "40px", margin: "20px", color: "white", fontWeight: "100" }}>Tâches</p>
                             </Link>
-                            <Link to={`/gift`}
-                                  style={{marginLeft: "50px", textDecoration: "none", borderBottom: "2px solid white"}}>
-                                <p style={{
-                                    fontSize: "40px",
-                                    margin: "20px",
-                                    color: "white",
-                                    fontWeight: "100"
-                                }}>({giftData.length}) Cadeaux</p>
+                            <Link to={`/gift`} style={{ marginLeft: "50px", textDecoration: "none", borderBottom: "2px solid white" }}>
+                                <p style={{ fontSize: "40px", margin: "20px", color: "white", fontWeight: "100" }}>({giftData.length}) Cadeaux</p>
                             </Link>
                         </div>
                         <Grid>
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Grid, Fab, Dialog, DialogContent } from '@mui/material';
+import { Grid, Fab, Dialog, DialogContent } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import Tasklist from './Tasklist';
 import Weather from '../Weather';
 import sphere from "../../assets/sphère3d.png";
 import { Link } from 'react-router-dom';
 import '../../styles/style.css';
-import CircularProgress from "@mui/material/CircularProgress";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Addtask from '../Task/Addtask';
 import Header from '../Header';
+import CircularProgress from "@mui/material/CircularProgress";
 
 function Task() {
     const [taskData, setTaskData] = useState([]);
@@ -54,46 +54,24 @@ function Task() {
             <Header/>
             {loading ? (
                 <Grid>
-                    <Box sx={{display: "flex"}}>
-                        <CircularProgress/>
-                    </Box>
+                    <CircularProgress/>
                 </Grid>
             ) : (
-                <div style={{
-                    position: 'fixed',
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    display: 'flex',
-                    justifyContent: 'space-between'
-                }}
-                     className="container_weather_taskgift">
-                    <div style={{display: 'flex', flexDirection: 'column', marginLeft: "15%"}}
-                         className="weather_sphere">
+                <div style={{ position: 'fixed', left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'space-between' }} className="container_weather_taskgift">
+                    <div style={{ display: 'flex', flexDirection: 'column', marginLeft: "15%" }} className="weather_sphere">
                         <Weather/>
-                        <img src={sphere} style={{width: "20vw", borderRadius: 100, marginTop: "50px"}} alt="sphere"/>
+                        <img src={sphere} style={{ width: "20vw", borderRadius: 100, marginTop: "50px" }} alt="sphere"/>
                     </div>
-                    <div style={{display: 'flex', flexDirection: 'column'}} className="container_task_gift">
-                        <div style={{display: "flex", alignItems: "center"}} className="container_add">
+                    <div style={{ display: 'flex', flexDirection: 'column' }} className="container_task_gift">
+                        <div style={{ display: "flex", alignItems: "center" }} className="container_add">
                             <Fab color="primary" aria-label="add" onClick={handleOpenModal}>
                                 <AddIcon/>
                             </Fab>
-                            <Link to={`/task`}
-                                  style={{marginLeft: "50px", textDecoration: "none", borderBottom: "2px solid white"}}>
-                                <p style={{
-                                    fontSize: "40px",
-                                    margin: "20px",
-                                    color: "white",
-                                    fontWeight: "100"
-                                }}>({taskData.length}) Tâches</p>
+                            <Link to={`/task`} style={{ marginLeft: "50px", textDecoration: "none", borderBottom: "2px solid white" }}>
+                                <p style={{ fontSize: "40px", margin: "20px", color: "white", fontWeight: "100" }}>({taskData.length}) Tâches</p>
                             </Link>
-                            <Link to={`/gift`} style={{marginLeft: "50px", textDecoration: "none"}}>
-                                <p style={{
-                                    fontSize: "40px",
-                                    margin: "20px",
-                                    color: "white",
-                                    fontWeight: "100"
-                                }}>Cadeaux</p>
+                            <Link to={`/gift`} style={{ marginLeft: "50px", textDecoration: "none" }}>
+                                <p style={{ fontSize: "40px", margin: "20px", color: "white", fontWeight: "100" }}>Cadeaux</p>
                             </Link>
                         </div>
                         <Grid>
