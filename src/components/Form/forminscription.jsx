a8305c0463d8abb92cf2a366bd120f5a8f7b54e0
yan <yan.ledoux@icloud.com>
2023-05-21 22:26:18 +0200

    add images

--- src/components/Form/forminscription.jsx
+++ src/components/Form/forminscription.jsx
@@ -1,14 +1,11 @@
-import React, {useState} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Link} from "react-router-dom";
-import {faEnvelope, faLock, faUser} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/logoeworld.png";
-import "../../styles/style.css"
-
+import "../../styles/style.css";
 
 function UserRegistration() {
-    // const [nom, setNom] = useState('');
-    // const [prenom, setPrenom] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
@@ -31,15 +28,16 @@ function UserRegistration() {
             });
 
             if (response.ok) {
-                response.json().then((data) => {
-                    localStorage.setItem('user_id', data.id);
-                    localStorage.setItem('username', data.username);
-                    localStorage.setItem('roles', data.roles[0]);
-                    console.log('Connexion réussie !');
-                    window.location.href = '/task';
-                }); 
+                const data = await response.json();
+
+                localStorage.setItem('user_id', data.id);
+                localStorage.setItem('username', data.username);
+                localStorage.setItem('roles', data.roles[0]);
+                localStorage.setItem('imageUrl', data.imageUrl); // Stocke l'URL de l'image dans le localStorage
+
+                console.log('Inscription réussie !');
+                window.location.href = '/task';
             } else {
-                // Une erreur s'est produite lors de l'inscription
                 const errorData = await response.json();
                 setError(errorData.message);
             }
@@ -50,9 +48,9 @@ function UserRegistration() {
     };
 
     return (
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <img src={logo} alt='logo e-world' className="logoeworld"/>
-            <h1 style={{fontSize: '40px', marginBottom: '20px', color: '#5E8CFF',}}>Inscription</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <img src={logo} alt='logo e-world' className="logoeworld" />
+            <h1 style={{ fontSize: '40px', marginBottom: '20px', color: '#5E8CFF', }}>Inscription</h1>
             {error && (
                 <p style={{
                     color: 'red',
@@ -67,42 +65,31 @@ function UserRegistration() {
                     {error}
                 </p>
             )}
-            <form onSubmit={handleSubmit}
-                  style={{display: 'flex', flexDirection: 'column', alignItems: 'center', color: "black"}}>
-                {/* <label style={{marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px"}}>
-                    <FontAwesomeIcon icon={faUser} style={{marginRight: '8px'}}/>
+            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: "black" }}>
+                <label style={{ marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px" }}>
+                    <FontAwesomeIcon icon={faUser} style={{ marginRight: '8px' }} />
                     <input
-                        style={{padding: '8px', border: "none"}}
-                        placeholder="Nom"
-                        type="text"
-                        value={nom}
-                        onChange={(e) => setNom(e.target.value)}
-                    />
-                </label> */}
-                <label style={{marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px"}}>
-                    <FontAwesomeIcon icon={faUser} style={{marginRight: '8px'}}/>
-                    <input
-                        style={{padding: '8px', border: "none"}}
+                        style={{ padding: '8px', border: "none" }}
                         placeholder="Username"
                         type="text"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
                 </label>
-                <label style={{marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px"}}>
-                    <FontAwesomeIcon icon={faEnvelope} style={{marginRight: '8px'}}/>
+                <label style={{ marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px" }}>
+                    <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px' }} />
                     <input
-                        style={{padding: '8px', border: "none"}}
+                        style={{ padding: '8px', border: "none" }}
                         placeholder="Email"
                         type="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
                 </label>
-                <label style={{marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px"}}>
-                    <FontAwesomeIcon icon={faLock} style={{marginRight: '8px'}}/>
+                <label style={{ marginBottom: '15px', padding: "10px", background: "white", borderRadius: "20px" }}>
+                    <FontAwesomeIcon icon={faLock} style={{ marginRight: '8px' }} />
                     <input
-                        style={{padding: '8px', border: "none"}}
+                        style={{ padding: '8px', border: "none" }}
                         placeholder="Mot de passe"
                         type="password"
                         value={password}
@@ -110,27 +97,11 @@ function UserRegistration() {
                     />
                 </label>
                 <Link to='/'>
-                    <button style={{
-                        margin: '20px',
-                        color: '#007BFF',
-                        cursor: 'pointer',
-                        background: "none",
-                        border: "none"
-                    }}>
+                    <button style={{ margin: '20px', color: '#007BFF', cursor: 'pointer', background: "none", border: "none" }}>
                         Retour sur eworld-front.vercel.app
                     </button>
                 </Link>
-                <button
-                    style={{
-                        padding: '10px 15px',
-                        background: '#5E8CFF',
-                        color: 'black',
-                        border: 'none',
-                        borderRadius: '20px',
-                        cursor: 'pointer',
-                    }}
-                    type="submit"
-                >
+                <button style={{ padding: '10px 15px', background: '#5E8CFF', color: 'black', border: 'none', borderRadius: '20px', cursor: 'pointer' }} type="submit">
                     S'inscrire
                 </button>
             </form>
@@ -139,8 +110,3 @@ function UserRegistration() {
 }
 
 export default UserRegistration;
-
-
-
-
-
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import {Button} from '@mui/material';
 import logo from "../assets/logoeworld.png";
-import person from "../assets/logosneakbyyan.png";
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../styles/style.css';
 import CurrentDate from "./CurrentDate";
 import SettingsIcon from '@mui/icons-material/Settings';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Header() {
-
     const [username, setUsername] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
 
     useEffect(() => {
         const storedUsername = localStorage.getItem('username');
+        const storedImageUrl = localStorage.getItem('imageUrl');
+        setImageUrl(storedImageUrl);
+        console.log("image :", storedImageUrl);
         setUsername(storedUsername);
     }, []);
 
@@ -21,14 +23,20 @@ function Header() {
         <div className="divHeader">
             <h1>Bienvenue,<br/> {username}</h1>
             <Link to={`/task`} style={{display: "flex", justifyContent: "center"}}>
-            <img src={logo} alt='logo e-world' className="logoeworldtask"/>
+                <img src={logo} alt='logo e-world' className="logoeworldtask"/>
             </Link>
-            <Link to={`/Profil`} style={{display:"none"}} className="settingsmobil">
+            <Link to={`/Profil`} style={{display: "none"}} className="settingsmobil">
                 <SettingsIcon style={{color: "white"}}/>
             </Link>
             <div style={{display: 'flex', flexDirection: 'column', alignItems: "center"}} className="headerProfil">
                 <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
-                    <img src={person} style={{width: '15%', borderRadius: 100, marginBottom: "10px", marginRight: "10px"}} alt="imgPersonne"/>
+                    {imageUrl && (
+                        <img
+                            src={imageUrl}
+                            style={{width: '15%', borderRadius: 100, marginBottom: "10px", marginRight: "10px"}}
+                            alt="imgPersonne"
+                        />
+                    )}
                     <Link to={`/Profil`}>
                         <SettingsIcon style={{color: "white"}}/>
                     </Link>
