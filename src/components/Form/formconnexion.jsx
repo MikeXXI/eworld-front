60402a14b9ee1ac7e8cff3b1d7b3bdbe0360b914
Mickael <mickael.djegherif@outlook.fr>
2023-05-21 22:31:22 +0200

    with get img Url

--- src/components/Form/formconnexion.jsx
+++ src/components/Form/formconnexion.jsx
@@ -30,6 +30,7 @@ function UserLogin() {
                     localStorage.setItem('user_id', data.id);
                     localStorage.setItem('username', data.username);
                     localStorage.setItem('roles', data.roles[0]);
+                    localStorage.setItem('imageUrl', data.imageUrl); 
                     console.log('Connexion réussie !');
                     window.location.href = '/task';
                 });           
