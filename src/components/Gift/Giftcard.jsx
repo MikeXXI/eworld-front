3af38728f3a40bd6f0069951031725aff14684d4
yan <yan.ledoux@icloud.com>
2023-05-22 13:18:10 +0200

    update edit task and gift

--- src/components/Gift/Giftcard.jsx
+++ src/components/Gift/Giftcard.jsx
@@ -1,37 +1,73 @@
 import React, {useEffect, useState} from "react";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Grid, Modal, Button } from "@mui/material";
-import '../../styles/style.css';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import {Grid, Modal, Button, TextField} from "@mui/material";
+import "../../styles/style.css";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
-function Giftcard({ id, name, adress, price, description }) {
+function Giftcard({id, name, adress, price, description}) {
     const [open, setOpen] = useState(false);
-    const [imageUrl, setImageUrl] = useState('');
+    const [editing, setEditing] = useState(false);
+    const [editedName, setEditedName] = useState(name);
+    const [editedAddress, setEditedAddress] = useState(adress);
+    const [editedPrice, setEditedPrice] = useState(price);
+    const [editedDescription, setEditedDescription] = useState(description);
+    const [imageUrl, setImageUrl] = useState("");
 
     useEffect(() => {
-        const storedImageUrl = localStorage.getItem('imageUrl');
+        const storedImageUrl = localStorage.getItem("imageUrl");
         setImageUrl(storedImageUrl);
     }, []);
 
     const handleSubmit = (giftId) => {
         fetch(`https://eworld-api.osc-fr1.scalingo.io/api/gifts/${giftId}`, {
-            method: 'DELETE'
+            method: "DELETE",
         })
-            .then(response => {
+            .then((response) => {
                 if (response.ok) {
                     console.log("Gift deleted successfully");
                     handleClose();
                     window.location.reload(); // Refresh the page
                 } else {
-                    throw new Error('Error deleting gift');
+                    throw new Error("Error deleting gift");
                 }
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error);
-                alert('Erreur lors de la suppression du cadeau');
+                alert("Erreur lors de la suppression du cadeau");
+            });
+    };
+
+    const handleUpdate = (id) => {
+        const updatedGift = {
+            name: editedName,
+            address: editedAddress,
+            price: editedPrice,
+            description: editedDescription,
+        };
+
+        fetch(`https://eworld-api.osc-fr1.scalingo.io/api/gifts/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedGift),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    console.log("Gift updated successfully");
+                    setEditing(false);
+                    window.location.reload();
+                } else {
+                    throw new Error("Error updating gift");
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                alert("Erreur lors de la mise à jour du cadeau");
             });
     };
 
@@ -43,10 +79,19 @@ function Giftcard({ id, name, adress, price, description }) {
         setOpen(false);
     };
 
+    const handleEdit = () => {
+        setEditing(true);
+    };
+
+    const handleCancel = () => {
+        setEditing(false);
+    };
+
+
     return (
         <Grid
             sx={{
-                position: 'center',
+                position: "center",
                 width: 1500,
             }}
             className="gridCard"
@@ -55,15 +100,30 @@ function Giftcard({ id, name, adress, price, description }) {
                 <CardContent>
                     <div className="titleSup">
                         {imageUrl && (
-                            <img src={imageUrl} style={{ width: '8%', borderRadius: 100, marginRight: "20px" }} alt="imgPersonne" />
+                            <img
+                                src={imageUrl}
+                                style={{width: "8%", borderRadius: 100, marginRight: "20px"}}
+                                alt="imgPersonne"
+                            />
                         )}
-                        <p style={{ fontSize: "20px", fontWeight: "bold" }}>{name}</p>
-                        <button className="buttonsupp" onClick={handleOpen}>
-                            <DeleteIcon />
-                        </button>
+                        <p style={{fontSize: "20px", fontWeight: "bold"}}>{name}</p>
+                        <div>
+                            <button className="buttonsupp" onClick={handleEdit}>
+                                <EditIcon/>
+                            </button>
+                            <button className="buttonsupp" onClick={handleOpen}>
+                                <DeleteIcon/>
+                            </button>
+                        </div>
                     </div>
                     <CardMedia
-                        sx={{ height: 120, width: 175, margin: 'auto', padding: 2, borderRadius: "20px" }}
+                        sx={{
+                            height: 120,
+                            width: 175,
+                            margin: "auto",
+                            padding: 2,
+                            borderRadius: "20px",
+                        }}
                         image={adress}
                         title={adress}
                     />
@@ -87,6 +147,54 @@ function Giftcard({ id, name, adress, price, description }) {
                         Supprimer
                     </Button>
                     <Button variant="contained" onClick={handleClose}>
+                        Annuler
+                    </Button>
+                </div>
+            </Modal>
+            <Modal
+                open={editing}
+                onClose={handleCancel}
+                aria-labelledby="modal-title"
+                aria-describedby="modal-description"
+            >
+                <div className="modal-content">
+                    <h1 id="modal-title">Modifier le cadeau</h1>
+                    <TextField
+                        label="Nom"
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Adresse"
+                        value={editedAddress}
+                        onChange={(e) => setEditedAddress(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Prix"
+                        value={editedPrice}
+                        onChange={(e) => setEditedPrice(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Description"
+                        value={editedDescription}
+                        onChange={(e) => setEditedDescription(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                    />
+                    <Button variant="contained" onClick={() => handleUpdate(id)}>
+                        Modifier
+                    </Button>
+                    <Button variant="contained" onClick={handleCancel}>
                         Annuler
                     </Button>
                 </div>
--- src/components/Task/Taskcard.jsx
+++ src/components/Task/Taskcard.jsx
@@ -1,13 +1,17 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Grid, Modal, Button } from "@mui/material";
+import { Grid, Modal, Button, TextField } from "@mui/material";
 import '../../styles/style.css';
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 function Taskcard({ id, title, description }) {
     const [open, setOpen] = useState(false);
+    const [editing, setEditing] = useState(false);
+    const [editedName, setEditedName] = useState(title);
+    const [editedDescription, setEditedDescription] = useState(description);
     const [imageUrl, setImageUrl] = useState('');
 
     useEffect(() => {
@@ -22,7 +26,7 @@ function Taskcard({ id, title, description }) {
             .then(response => {
                 if (response.ok) {
                     handleClose();
-                    window.location.reload(); // Refresh the page
+                    window.location.reload(); // Rafraîchir la page
                 } else {
                     throw new Error('Error deleting task');
                 }
@@ -32,12 +36,48 @@ function Taskcard({ id, title, description }) {
             });
     };
 
-    const modalOpen = () => {
+    const handleUpdate = (id) => {
+        const updatedTask = {
+            title: editedName,
+            description: editedDescription,
+        };
+
+        fetch(`https://eworld-api.osc-fr1.scalingo.io/api/tasks/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedTask),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    console.log("Task updated successfully");
+                    setEditing(false);
+                    window.location.reload();
+                } else {
+                    throw new Error("Error updating task");
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                alert("Erreur lors de la mise à jour de la tâche");
+            });
+    };
+
+    const handleOpen = () => {
         setOpen(true);
     };
 
     const handleClose = () => {
         setOpen(false);
+    };
+
+    const handleEdit = () => {
+        setEditing(true);
+    };
+
+    const handleCancel = () => {
+        setEditing(false);
     };
 
     return (
@@ -51,17 +91,22 @@ function Taskcard({ id, title, description }) {
             <Card className="cardContent">
                 <CardContent>
                     <div className="titleSup">
-                        <div style={{ margin: 0, display: "flex", alignItems: "center", textTransform: "uppercase", fontWeight: "900"}}>
+                        <div style={{ margin: 0, display: "flex", alignItems: "center", textTransform: "uppercase", fontWeight: "900" }}>
                             {imageUrl && (
-                            <img src={imageUrl} style={{ width: '8%', borderRadius: 100, marginRight: "20px" }} alt="imgPersonne" />
+                                <img src={imageUrl} style={{ width: '8%', borderRadius: 100, marginRight: "20px" }} alt="imgPersonne" />
                             )}
                             <div>
-                            {title}
+                                {title}
                             </div>
                         </div>
-                        <button className="buttonsupp" onClick={modalOpen}>
-                            <DeleteIcon />
-                        </button>
+                        <div>
+                            <button className="buttonsupp" onClick={handleEdit}>
+                                <EditIcon />
+                            </button>
+                            <button className="buttonsupp" onClick={handleOpen}>
+                                <DeleteIcon />
+                            </button>
+                        </div>
                     </div>
                     <Typography gutterBottom variant="h6" component="div" className="description">
                         {description}
@@ -80,6 +125,38 @@ function Taskcard({ id, title, description }) {
                         Supprimer
                     </Button>
                     <Button variant="contained" onClick={handleClose}>
+                        Annuler
+                    </Button>
+                </div>
+            </Modal>
+            <Modal
+                open={editing}
+                onClose={handleCancel}
+                aria-labelledby="modal-title"
+                aria-describedby="modal-description"
+            >
+                <div className="modal-content">
+                    <h1 id="modal-title">Modifier la tâche</h1>
+                    <TextField
+                        label="Nom"
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                    />
+                    <TextField
+                        label="Description"
+                        value={editedDescription}
+                        onChange={(e) => setEditedDescription(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        fullWidth
+                    />
+                    <Button variant="contained" onClick={() => handleUpdate(id)}>
+                        Modifier
+                    </Button>
+                    <Button variant="contained" onClick={handleCancel}>
                         Annuler
                     </Button>
                 </div>
